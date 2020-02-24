6ee56b961039a0e747bd0289562a5dc2fe9d1322
massquantity <wdmjjxg@163.com>
2020-02-24 13:33:10 +0800

    101

--- 101_Symmetric_Tree/101_2.py
+++ 101_Symmetric_Tree/101_2.py
@@ -1,18 +1,17 @@
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-    	if not root:  return True
-    	queue = list()
-    	while queue:
-    		first = queue.pop(0)
-    		second = queue.pop(1)
-    		if not first and not second:
-    			continue
-    		if not first or not second:
-    			return False
-    		if first.val != second.val:
-    			return False
-    		queue.append(first.left)
-    		queue.append(second.right)
-    		queue.append(first.right)
-    		queue.append(second.left)
-    	return True
+        if not root: return True
+        queue = list()
+        queue.append(root.left)
+        queue.append(root.right)
+        while queue:
+            first = queue.pop(0)
+            second = queue.pop(0)
+            if not first and not second: continue
+            if not first or not second:  return False
+            if first.val != second.val:  return False
+            queue.append(first.left)
+            queue.append(second.right)
+            queue.append(first.right)
+            queue.append(second.left)
+        return True
