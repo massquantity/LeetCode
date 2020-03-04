f1f1fcf4eebfc4007f7d324fa262a07a67b7d7c4
massquantity <wdmjjxg@163.com>
2020-03-04 13:55:49 +0800

    994

---
+++ 994_Rotting_Oranges/994_3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+        rotten = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 2}
+        fresh = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 1}
+        t = 0
+        while fresh:
+            if not rotten: return -1
+            rotten = {(i + di, j + dj) for i, j in rotten for di, dj in [(0, 1), (0, -1), (-1, 0), (1, 0)] if (i + di, j + dj) in fresh}
+            fresh -= rotten
+            t += 1
+        return t
