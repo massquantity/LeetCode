8910ede982891ee509cb278dccb5310727618e2e
massquantity <wdmjjxg@163.com>
2020-03-23 20:22:02 +0800

    008

---
+++ 008_String_to_Integer_(atoi)/008_1.py
@@ -0,0 +1,9 @@
+import re
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        return max(min(int(*re.findall('^[\+\-]?\d+', s.lstrip())), 2**31 - 1), -2**31)
+
+if __name__ == "__main__":
+    s = Solution()
+    s.myAtoi("-91283472332")
