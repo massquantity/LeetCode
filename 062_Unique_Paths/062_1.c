94f8c6f758fe9de7cf8a9caedb222feb9dd631b0
massquantity <wdmjjxg@163.com>
2020-03-30 18:20:08 +0800

    062

---
+++ 062_Unique_Paths/062_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int uniquePaths(int m, int n){
+    int dp[m][n];
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == 0 || j == 0) dp[i][j] = 1;
+            else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+    return dp[m - 1][n - 1];
+}
+
+/*
+static void traverse(struct TreeNode *tree) {
+    printf("%d ", tree->val);
+    if (tree->left)  traverse(tree->left);
+    if (tree->right) traverse(tree->right);
+}
+*/
+
+int main(void) {
+    int m = 7, n = 3;
+    printf("%d\n", uniquePaths(m, n));
+}
+
