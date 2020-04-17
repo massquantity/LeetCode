c7731c328b8d5e5508d2aa3628763bbb47804a4b
massquantity <wdmjjxg@163.com>
2020-04-17 13:03:51 +0800

    010

---
+++ 010_Regular_Expression_Matching/010_2.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = dict()
+        def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if j == len(p):
+                return i == len(s)
+            first_match = i < len(s) and p[j] in {s[i], '.'}
+            if j <= len(p) - 2 and p[j + 1] == '*':
+                ans = dp(i, j + 2) or \
+                    first_match and dp(i + 1, j)
+            else:
+                ans = first_match and dp(i + 1, j + 1)
+            memo[(i, j)]  = ans
+            return ans
+
+        return dp(0, 0)
+
+
+if __name__ == "__main__":
+    string = "aab"
+    pattern = "c*a*b"
+    print(Solution().isMatch(string, pattern))
