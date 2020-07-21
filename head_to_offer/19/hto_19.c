caeb026f4fb069bdbcd8db6130c1c85837cfb94e
massquantity <wdmjjxg@163.com>
2020-07-21 15:55:20 +0800

    hto_19

---
+++ head_to_offer/19/hto_19.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool isMatch(char *s, char *p) {
+    if (!*p) return !*s;
+    bool first_match = *s && (*s == *p || *p == '.');
+    if (strlen(p) >= 2 && *(p + 1) == '*')
+        return isMatch(s, p + 2) || (first_match && isMatch(s + 1, p));
+    else
+        return first_match && isMatch(s + 1, p + 1);
+}
+
+int main(void) {
+    char *s = "abb";
+    char *p = "*";
+    printf("%s\n", isMatch(s, p) ? "true" : "false");
+}
