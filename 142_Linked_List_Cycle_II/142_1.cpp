224121cb2e393e99e11f0f7c0e78431192c310f7
massquantity <wdmjjxg@163.com>
2020-07-21 19:01:33 +0800

    142

---
+++ 142_Linked_List_Cycle_II/142_1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        unordered_set<ListNode *> set;
+        while (head != nullptr) {
+            if (set.find(head) != set.end()) return head;
+            set.emplace(head);
+            head = head->next;
+        }
+        return nullptr;
+    }
+};
