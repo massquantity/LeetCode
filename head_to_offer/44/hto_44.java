694f51079eff1524ca6a2afd936e65353da1e69c
massquantity <wdmjjxg@163.com>
2020-08-06 23:56:06 +0800

    hto_44

---
+++ head_to_offer/44/hto_44.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findNthDigit(int n) {
+        int digit = 1;
+        long start = 1;
+        long count = 9;
+        while (n > count) {
+            n -= count;
+            digit++;
+            start *= 10;
+            count = 9 * start * digit;
+        }
+        long num = start + (n - 1) / digit;
+        return Long.toString(num).charAt((n - 1) % digit) - '0';
+    }
+}
