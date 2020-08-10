c39d3058a4c48b67f84e737f37a2af57fef8a2d1
massquantity <wdmjjxg@163.com>
2020-08-10 14:56:10 +0800

    change name

--- head_to_offer/52/hto_52.c
+++
@@ -1,8 +0,0 @@
-struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
-        struct ListNode *h1 = headA, *h2 = headB;
-        while (h1 != h2) {
-            h1 = h1 ? h1->next : headB;
-            h2 = h2 ? h2->next : headA;
-        }
-        return h1;
-}
