e85e9abb5c170c9df21e244c56fcb23331f231f4
massquantity <wdmjjxg@163.com>
2020-08-24 20:49:41 +0800

    hto_68-2

---
+++ hto/68-2/hto_68-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return nullptr;
+        if (root == p || root == q) return root;
+        TreeNode *left  = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) return root;
+        else if (left)     return left;
+        else if (right)    return right;
+        return nullptr;
+    }
+};
