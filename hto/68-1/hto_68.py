c4f8608dea83270499d4821546244d662cf739db
massquantity <wdmjjxg@163.com>
2020-08-24 20:43:19 +0800

    hto_68

---
+++ hto/68-1/hto_68.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return None
+        if p == root or q == root or p.val < root.val < q.val or q.val < root.val < p.val:
+            return root
+        
+        if p.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return self.lowestCommonAncestor(root.right, p, q)
